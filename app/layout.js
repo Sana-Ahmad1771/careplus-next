c81aba230cbe93186f3cb70ee10a7ae7587ef5f1
Sana Ahmad <sanaahmad17771@gmail.com>
2025-08-08 16:50:19 +0500

    added title to OG

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ export const metadata = {
   },
   description: "Your trusted healthcare partner",
   openGraph: {
+    title: "CarePlus",
     description: "Your trusted healthcare partner",
     images: [
       {
