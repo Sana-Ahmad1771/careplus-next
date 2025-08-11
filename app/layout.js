a68100094ecf639ec72c4fb73817829e5e8e6bf3
Sana Ahmad <sanaahmad17771@gmail.com>
2025-08-11 09:53:45 +0500

    meta data insertion

--- app/layout.js
+++ app/layout.js
@@ -23,8 +23,8 @@ export const metadata = {
     images: [
       {
         // url: "/og",
-        // width: 1200,
-        // height: 630,
+        width: 1200,
+        height: 630,
       },
     ],
   },
